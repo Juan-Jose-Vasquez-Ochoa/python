c8ee83851c45830a294eeab7f231f48f1cb12e15
Juan-Jose-Vasquez-Ochoa <juanjosevasqiez8@gmail.com>
2025-05-15 15:44:48 -0500

    Update taller.py

--- taller.py
+++ taller.py
@@ -1,36 +1,51 @@
-books = {}
-
-id = 1
-
+books = []
+category = ""
 def add_book(books):
-
-    cont = True
-    global id
-    
-    
+    id = 0
     title = input("Title of the book: ")
     author = input("Author of the book: ")
-    
-    while cont:
-
-        try:
-
-            year = int(input("Year of publication: "))
-            if 1500 > year > 2025:
-                print(" Error: The year of publication must be between 1500 and 2025")
-                continue
-            cont = False
-        
-        except ValueError:
-            print(" Error: Ingresa valores numéricos válidos." )
-        year = str(year)
-
-    category = input("What is the book's category?: ")
-    state = input("What is the book's state? (avaliable/borrowed): ")
-    books[id] = {"title": title, "author": author, "year": year, "category": category,"state": state}
-    print(" Libro añadido correctamente.")
-    id += 1
-
+    while True:
+        year = input("Year of publication: ")
+        if year.isdigit() and int(year) >= 1500 and int(year) <= 2025:
+            while True:
+                print("Select the type of book ")
+                print("[1] Fiction")
+                print("[2] No fiction")
+                print("[3] Infantila")
+                print("[4] Educative")
+                category = input("What is the book's category?: ")
+                if category == "1":
+                    category = "Fiction"
+                    break
+                elif category == "2":
+                    category = "No fiction"
+                    break
+                elif category == "3":
+                    category = "Infantila"
+                    break
+                elif category == "4":
+                    category = "Educative"
+                    break
+                else:
+                    print("Invalid value")
+            while True:
+                print("Select the type of book ")
+                print("[1] Avaliable")
+                print("[2] Borrowed")
+                state = input("What is the book's state?: ")
+                if state == "1":
+                    state = "Avaliable"
+                    break
+                elif state == "2":
+                    state = "Borrowed"
+                    break
+                else:
+                    print("Invalid value")
+            id += 1
+            book = {"id": id, "title": title, "author": author, "year": year, "category": category,"state": state}
+            books.append(book) 
+            print(" Libro añadido correctamente.")
+            print(books)     
+        else:
+            print(" Error: The year of publication must be between 1500 and 2025")         
 add_book(books)
-
-print(books)
